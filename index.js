8a6e37bf94866b812bb73537c91033eb1245f43c
Tamoy <sinclairtamoy@gmail.com>
2022-01-13 23:27:04 -0500

    commit tree cb8f39644bb36bf679d7c9fd52588b9b082f9a13

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(distance){
+  if (distance <= 400){
+    return "This one is on me!"
+  }
+  if (distance > 2500){
+    return 'No can do.'}
+
+  if (distance > 2000){
+    return 'I will gladly take your thirty bucks.'
+  }
+  
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function ternaryCheckCity(city){
+    return (city === "NYC" ? "Ok, sounds good." : 'No go.')
+  
+} 
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  switch (tip){
+    case 'generous' :
+    return ('Thank you so much.')
+    break;
+    case 'not as generous' :
+    return ('Thank you.')
+    break;
+    default :
+    return ('Bye.')
+  }
+  
 }
